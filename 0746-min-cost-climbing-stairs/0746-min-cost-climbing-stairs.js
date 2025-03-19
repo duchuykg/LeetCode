8b11c7d785490dc24e5d7ccae9f05941f426bed2
Phạm Hoàng Đức Huy <100073936+duchuykg@users.noreply.github.com>
2025-03-19 11:32:22 +0700

    Time: 2 ms (53.63%), Space: 54.6 MB (21.44%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let min = 0;
+    let l1 = cost[0];
+    let l2 = cost[1];
+
+    for (let i = 2; i < cost.length; i++){
+        min = Math.min(l1, l2) + cost[i];
+        l1 = l2;
+        l2 = min;
+    }
+
+    return Math.min(l1, l2);
+};
